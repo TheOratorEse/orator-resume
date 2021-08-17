8ecddf6615dc333ea3645f101bc5df567bd1d51c
Eseiwi <88969604+TheOratorEse@users.noreply.github.com>
2021-08-18 00:29:35 +0100

    Add files via upload

---
+++ lib/thank-you.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+
+<title>Form submitted successfully</title>
+    <link rel="shortcut icon" href="img/favicon.ico">
+<link href="\css/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+<div class="container"> <p> Thank you, <?php echo $_POST["sender_name"]; ?>.  Your Response has been successfully received, <br></br> we will get in touch </p>
+</div>
+
+
+</body>
+</html>
