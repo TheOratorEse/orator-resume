fde09345239eec43bc1026d00b29af829c5e3e50
Eseiwi <88969604+TheOratorEse@users.noreply.github.com>
2021-08-18 03:09:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head> 
 
 <title>Orator | Resume page</title>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="shortcut icon" href="img/favicon.ico">
 <link href="css/style.css" rel="stylesheet">
 
